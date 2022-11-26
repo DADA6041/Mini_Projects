f433e6b712065cf5599e5f78c96ba5a8543c0198
DADAKIM <d.ya96v@gmail.com>
2022-11-27 02:04:46 +0900

    :wrench: 게임오버구현 & 새 계속생성되는 오류 픽스

--- FlappyBird/src/app.js
+++ FlappyBird/src/app.js
@@ -4,18 +4,22 @@ const gameArea = document.querySelector('.area-game');
 const gameMsg = document.querySelector('.msg-game');
 
 let keys = {};
-let bird = document.createElement('div');
-let wing = document.createElement('div');
 let player = {
     x: 0,
     y: 0,
     speed: 4,
-    score: 0
+    score: 0,
+    isplay: false
 };
 
 function gameStart() {
+    player.isplay = true;
+    player.score = 0;
     gameMsg.classList.add('hidden');
     startBtn.classList.add('hidden');
+    score.classList.remove('hidden');
+    let bird = document.createElement('div');
+    let wing = document.createElement('div');
     bird.setAttribute('class', 'bird');
     wing.setAttribute('class', 'wing');
     bird.appendChild(wing);
@@ -28,47 +32,68 @@ function gameStart() {
 }
 
 function playGame(){
-    let move = false;
-    if(keys.ArrowLeft && player.x > -20){
-        player.x -= player.speed;
-        move = true;
-    }
-    if(keys.ArrowRight && player.x < gameArea.offsetWidth - bird.offsetWidth + 20){
-        player.x += player.speed;
-        move = true;
-    }
-    if((keys.ArrowUp || keys.Space) && player.y > -20){
-        player.y -= player.speed * 4;
-        move = true;
-    }
-    if(keys.ArrowDown){
-        player.y += player.speed;
-        move = true;
-    }
-    if(move) {
-        wing.pos = wing.pos === 10? 15 : 10;
-        wing.style.top = wing.pos + "px";
-    }
-
-    if (player.y < gameArea.offsetHeight - bird.offsetHeight + 20) {
+    if(player.isplay){
+        let bird = document.querySelector('.bird');
+        let wing = document.querySelector('.wing');
+        let move = false;
+        if(keys.ArrowLeft && player.x > -20){
+            player.x -= player.speed;
+            move = true;
+        }
+        if(keys.ArrowRight && player.x < gameArea.offsetWidth - bird.offsetWidth + 20){
+            player.x += player.speed;
+            move = true;
+        }
+        if((keys.ArrowUp || keys.Space) && player.y > -20){
+            player.y -= player.speed * 4;
+            move = true;
+        }
+        if(keys.ArrowDown && player.y < gameArea.offsetHeight - bird.offsetHeight){
+            player.y += player.speed;
+            move = true;
+        }
+        if(move) {
+            wing.pos = wing.pos === 10? 15 : 10;
+            wing.style.top = wing.pos + "px";
+        }
+    
         player.y += player.speed * 2;
-    }
 
-    bird.style.left = player.x + "px";
-    bird.style.top = player.y + "px";
-    window.requestAnimationFrame(playGame);
-    player.score += 1;
-    score.innerText = `SCORE : ${player.score}`;
+        if(player.y > gameArea.offsetHeight){
+            gameOver();
+        }
+    
+        bird.style.left = player.x + "px";
+        bird.style.top = player.y + "px";
+        window.requestAnimationFrame(playGame);
+        player.score += 1;
+        score.innerText = `SCORE : ${player.score}`;
+    }
+}
+
+function gameOver(){
+    let bird = document.querySelector('.bird');
+    let wing = document.querySelector('.wing');
+    player.isplay = false;
+    score.classList.add('hidden');
+    gameMsg.classList.remove('hidden');
+    gameMsg.innerHTML = `
+    GAME OVER <br>
+    SCORE : ${player.score}점 <br>
+    <p class="restart">다시 시작하려면 여기를 누르세요 :)</p>
+    `
+    bird.remove();
+    wing.remove();
 }
 
 function pressOn(e){
     keys[e.code] = true;
-    console.log(keys);
+
 }
 
 function pressOff(e){
     keys[e.code] = false;
-    console.log(keys);
+
 }
 
 startBtn.addEventListener('click', gameStart);
