5d8df4ec5833e174511c99ef7e39b83e0d7afe9e
DADAKIM <d.ya96v@gmail.com>
2022-12-31 20:25:49 +0900

    :tada: 커스텀 버킷리스트 셋팅

---
+++ CustoList/src/app.js
@@ -0,0 +1,13 @@
+const main = document.querySelector('.cont-bucket');
+const userInp = main.querySelector('.cont-list .cont-input input');
+const addBtn = main.querySelector('.cont-list .cont-input button');
+const listWish = main.querySelector('.list-wish');
+
+// 버킷리스트 추가 버튼 이벤트
+addBtn.addEventListener('click', createWishList);
+
+// 엔터키 이벤트
+
+function createWishList(){
+    
+}
